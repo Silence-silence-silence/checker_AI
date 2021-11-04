8b269e17902b84e08630f4138de74eddee6badaa
Haoting Ni <ni.haoting@outlook.com>
2021-11-03 18:57:03 -0700

    commit

---
+++ Checkers_Student-master/src/checkers-cpp/cmake-build-release-remote-host/CMakeFiles/Checker_Teacher.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/haotinn/hw/hw2/Board.cpp" "/home/haotinn/hw/hw2/cmake-build-release-remote-host/CMakeFiles/Checker_Teacher.dir/Board.cpp.o"
+  "/home/haotinn/hw/hw2/Checker.cpp" "/home/haotinn/hw/hw2/cmake-build-release-remote-host/CMakeFiles/Checker_Teacher.dir/Checker.cpp.o"
+  "/home/haotinn/hw/hw2/GameLogic.cpp" "/home/haotinn/hw/hw2/cmake-build-release-remote-host/CMakeFiles/Checker_Teacher.dir/GameLogic.cpp.o"
+  "/home/haotinn/hw/hw2/ManualAI.cpp" "/home/haotinn/hw/hw2/cmake-build-release-remote-host/CMakeFiles/Checker_Teacher.dir/ManualAI.cpp.o"
+  "/home/haotinn/hw/hw2/Move.cpp" "/home/haotinn/hw/hw2/cmake-build-release-remote-host/CMakeFiles/Checker_Teacher.dir/Move.cpp.o"
+  "/home/haotinn/hw/hw2/StudentAI.cpp" "/home/haotinn/hw/hw2/cmake-build-release-remote-host/CMakeFiles/Checker_Teacher.dir/StudentAI.cpp.o"
+  "/home/haotinn/hw/hw2/Utils.cpp" "/home/haotinn/hw/hw2/cmake-build-release-remote-host/CMakeFiles/Checker_Teacher.dir/Utils.cpp.o"
+  "/home/haotinn/hw/hw2/main.cpp" "/home/haotinn/hw/hw2/cmake-build-release-remote-host/CMakeFiles/Checker_Teacher.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
